2a86a3d6d26ea4fdb90039c1ef8f9917b5f37601
Macho99 <pes4825@naver.com>
2024-03-20 13:56:36 +0900

    feat : 배지어 곡선으로 궤도 그리기

---
+++ Assets/Scripts/BezierCurves/BezierCurve.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class BezierCurve : MonoBehaviour 
+{
+
+	public Vector3[] points;
+	
+	public Vector3 GetPoint (float t) 
+	{
+		return transform.TransformPoint(Bezier.GetPoint(points[0], points[1], points[2], points[3], t));
+	}
+	
+	public Vector3 GetVelocity (float t) 
+	{
+		return transform.TransformPoint(Bezier.GetFirstDerivative(points[0], points[1], points[2], points[3], t)) - transform.position;
+	}
+	
+	public Vector3 GetDirection (float t) 
+	{
+		return GetVelocity(t).normalized;
+	}
+
+	public float GetTotalLength(int nrOfSegments)
+	{
+		float totalLength = 0;
+		float t = 1.0f/nrOfSegments;
+		Vector3 p0,p1;
+
+		for(int i=0; i<nrOfSegments; i++)
+		{
+			p0 = GetPoint( t*i );
+			p1 = GetPoint( t*(i+1));
+			totalLength += Vector3.Distance(p0,p1);
+		}
+		return totalLength;
+	}
+	
+	public void Reset () 
+	{
+		points = new Vector3[] 
+		{
+			new Vector3(1f, 0f, 0f),
+			new Vector3(2f, 0f, 0f),
+			new Vector3(3f, 0f, 0f),
+			new Vector3(4f, 0f, 0f)
+		};
+	}
+}
