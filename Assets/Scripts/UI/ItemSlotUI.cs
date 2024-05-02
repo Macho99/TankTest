337fb77c9ce2418fdea529f2f67908c8470e090b
KSH951007 <katafi6@naver.com>
2024-05-03 04:17:37 +0900

    feat. 슬릇 구성

--- Assets/Scripts/UI/ItemSlotUI.cs
+++ Assets/Scripts/UI/ItemSlotUI.cs
@@ -6,13 +6,20 @@ using UnityEngine.UI;
 
 public class ItemSlotUI : MonoBehaviour
 {
+    public enum ItemSlotType { Inventory, Equipment }
     public enum DurabilityState { Lot, Normal, Few }
+
+    [SerializeField] private Sprite itemEmptyIcon;
+    [SerializeField] private Image itemIconImage;
     [SerializeField] private TextMeshProUGUI itemCountTMP;
     [SerializeField] private GameObject durabilityObj;
     [SerializeField] private Image durabilityAmountImg;
     [SerializeField] private Sprite[] durabilityAmountImage;
 
 
+    private bool isEmpty;
+
+    public ItemSlotType itemSlotType { get; private set; }
 
     // Start is called before the first frame update
     void Start()
@@ -24,5 +31,23 @@ public class ItemSlotUI : MonoBehaviour
     void Update()
     {
 
+    }
+
+    public void SetItem(Item item)
+    {
+        isEmpty = item == null ? true : false;
+        CheckEmpty();
+
+
+
+      
+
+
+    }
+    private void CheckEmpty()
+    {
+        durabilityObj.SetActive(!isEmpty);
+        itemCountTMP.enabled = !isEmpty;
+        itemIconImage.sprite = isEmpty ? itemEmptyIcon : itemIconImage.sprite;
     }
 }
